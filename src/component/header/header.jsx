b69665b25610db4bea6ae54ba9e074ac0d7c733e
freebiker2000 <catalin.sandru88@gmail.com>
2019-01-06 14:08:29 +0000

    init componnts

---
+++ src/component/header/header.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class header__wrapper extends Component {
+  render() {
+    return(
+      <p>sdhvcgsh</p>
+    )
+  }
+}
+export default header__wrapper;
