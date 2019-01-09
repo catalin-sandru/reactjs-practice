49f194a8d1e632273f6440aabb05fb479d31a13b
freebiker2000 <catalin.sandru88@gmail.com>
2019-01-09 13:22:46 +0000

    using constructor to kep render clean

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import React, { Component} from 'react'
 class Layout extends Component {
+  constructor() {
+    super()
+    this.name = 'Catalin'
+  }
   render() {
       return(
-        <h1>It's Catalin!</h1>
+        <h1>It's {this.name}!</h1>
       )
   }
 }
